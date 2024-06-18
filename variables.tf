caee41d07f41842ccf872a70eedc5afee0a9acdc
Nchoana <111323955+Nchona@users.noreply.github.com>
2024-06-18 13:12:23 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,18 +3,3 @@ variable "region" {
   default     = "eu-west-3"
 }
 
-variable "source_bucket_name" {
-  description = "The name of the source S3 bucket"
-  type        = string
-  default     = "source-bucket-mitiga"
-}
-
-variable "assets_bucket_name" {
-  description = "The name of the assets S3 bucket"
-  type        = string
-  default     = "assets-bucket-mitiga"
-}
-
-# variable "s3_name" {
-#   type = string
-# }
